0272d6841a4c6a4b9f2d9516cad11b4e6a15cb56
risecommerce <risecommerce@gmail.com>
2020-06-21 20:04:29 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Risecommerce_Currencyformat',
+    __DIR__
+);
